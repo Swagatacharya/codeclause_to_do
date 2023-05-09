9b85da3d6f2c9f178609707086590f71a4e9b537
Swagat Acharya <96761760+Swagatacharya@users.noreply.github.com>
2023-05-09 14:22:14 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-function addTask() {
-    var taskInput = document.getElementById("taskInput");
-    var taskList = document.getElementById("taskList");
-    var task = taskInput.value;
-    if (task === "") {
-      alert("
